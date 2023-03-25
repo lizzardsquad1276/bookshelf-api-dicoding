60f38d65d52a23378b3f3d31db0ae83406cb965e
Ridho Hery <promumy1276@gmail.com>
2023-03-25 11:44:54 +0700

    initical code commit

---
+++ src/books.js
@@ -0,0 +1,3 @@
+const books = [];
+// aray diatas disiapkan untuk menampung data buku yang akan diinputkan oleh user
+module.exports = books;
---
+++ src/server.js
@@ -0,0 +1,23 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 9000,
+        host: 'localhost',
+        routes: {
+        },
+    });
+
+    server.route(routes)
+
+    await server.start();
+    console.log('Server berjalan pada %s', server.info.uri,);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init ();
